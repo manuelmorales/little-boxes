7ff46b09d060e4b589b9b9a554f69a71e29399cb
Manuel Morales <manuelmorales@gmail.com>
2015-12-11 11:53:02 +0000

    Created mini-example of real life objects

--- spec/little_boxes/box_spec.rb
+++ spec/little_boxes/box_spec.rb
@@ -1,94 +1,85 @@
 require_relative '../spec_helper'
 
 RSpec.describe 'Box' do
-  subject { my_box_class.new }
-
-  let(:my_box_class) do
-    m1 = Module.new do
-      include Box
-
-      get(:port) { '80' }
-      get(:log_level) { 'INFO' }
-    end
-
-    m2 = Module.new  do
-      include Box
-      get(:log_level_as_symbol) { log_level.to_sym }
-      let(:logger) { Logger.new log_level }
-      let(:server) { Server.new }
-
-      let(:users) { UsersBox.new }
-    end
-
-    Class.new.tap {|c| c.class_eval { include m1; include m2 } }
-  end
-
-  let(:users_box) do
-    Class.new do
-      include Box
-
-      get(:table_name) { 'users' }
-      get(:repo) { Repo.new logger }
+  def define_class name, &block
+    stub_const(name, Class.new).tap do |c|
+      c.class_eval(&block)
     end
   end
 
   before do
-    stub_const('Server', Class.new)
-    stub_const('UsersBox', users_box)
-
-    Server.class_eval do
+    define_class 'UsersCollection' do
       attr_accessor :logger
 
-      def initialize(args)
+      def initialize args
         args.each { |k,v| send "#{k}=", v }
       end
     end
 
-    stub_const 'Logger', Class.new
-
-    Logger.class_eval do
+    define_class 'Logger' do
       attr_accessor :level
 
-      def initialize(level)
-        @level = level
+      def initialize args
+        args.each { |k,v| send "#{k}=", v }
       end
     end
 
-    stub_const 'Repo', Class.new
-    Repo.class_eval do
-      attr_accessor :logger
-
-      def initialize(logger)
-        @logger = logger
+    define_class 'UsersApi' do
+      class << self
+        attr_accessor :logger
       end
     end
   end
 
-  it 'can return a value' do
-    expect(subject.port).to eq '80'
+  let(:users_collection) { UsersCollection.new logger: logger }
+  let(:logger) { Logger.new level: log_level }
+
+  let(:users_api) do
+    UsersApi.tap do |api|
+      api.logger = logger
+    end
   end
 
-  it 'can access values from other values in get' do
-    expect(subject.log_level_as_symbol).to eq subject.log_level.to_sym
+  def log_level
+    'INFO'
   end
 
-  it 'can access values from other values in let' do
-    expect(subject.logger.level).to eq subject.log_level
+  describe 'log_level' do
+    it 'is a new instance every time' do
+      expect(log_level).to eq 'INFO'
+      expect(log_level).not_to be log_level
+    end
+
+    it 'has the log_level' do
+      expect(logger.level).to eq log_level
+    end
+
+    it 'has memoized log_level' do
+      expect(logger.level).to be logger.level
+    end
   end
 
-  it 'doesn\'t share memoized values between instancens of the box' do
-    expect(subject.logger).not_to be my_box_class.new.logger
+  describe 'users_collection' do
+    it 'exists' do
+      expect(users_collection).to be_a UsersCollection
+    end
+
+    it 'has a logger' do
+      expect(users_collection.logger).to be_a Logger
+    end
+
+    it 'memoizes the logger' do
+      expect(users_collection.logger).to be users_collection.logger
+    end
   end
 
-  it 'can memoize values' do
-    expect(subject.logger).to be subject.logger
-  end
+  describe 'users_api' do
+    it 'exists' do
+      expect(users_api).to be UsersApi
+    end
 
-  it 'supports metaboxes' do
-    expect(subject.users.table_name).to eq 'users'
-  end
-
-  it 'supports getting values from the parent' do
-    expect(subject.users.repo.logger).to be_a Logger
+    it 'has a logger' do
+      expect(users_api.logger).to be_a Logger
+    end
   end
 end
