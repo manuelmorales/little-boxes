14375bcafc4cebe696e42e4660874f5bf3c58098
Manuel Morales <manuelmorales@gmail.com>
2015-12-11 14:22:14 +0000

    WIP

--- spec/little_boxes/box_spec.rb
+++ spec/little_boxes/box_spec.rb
@@ -9,10 +9,10 @@ RSpec.describe 'Box' do
 
   before do
     define_class 'UsersCollection' do
-      attr_accessor :logger
+      attr_accessor :config
 
-      def initialize args
-        args.each { |k,v| send "#{k}=", v }
+      def logger
+        config.logger       
       end
     end
 
@@ -36,55 +36,114 @@ RSpec.describe 'Box' do
 
     define_class 'UsersApi' do
       class << self
-        attr_accessor :logger
+        attr_accessor :config
+
+        def logger
+          config.logger
+        end
       end
     end
 
     define_class 'Server' do
-      def initialize config
-        @config = config
-      end
+      attr_accessor :config
 
       def logger
-        @config.logger
+        config.logger
       end
     end
+    
+    stub_const('Box', Module.new).tap do |m|
+      m.class_eval do
+        def self.included(base)
+          base.extend(ClassMethods)
+          base.class_eval do
 
-    define_class 'MainBox' do
-      class << self
-        def get name, &block
-          define_method name, &block
+          end
         end
 
-        def let name, &block
-          define_method name do
-            var_name = "@#{name}"
+        module InstanceMethods
+          def initialize(options = {})
+            self.parent = options[:parent]
+          end
+        end
 
-            if value = instance_variable_get(var_name)
-              value
-            else
-              instance_variable_set var_name, instance_eval(&block)
+        module ClassMethods
+          def get name, &block
+            define_method name, &block
+          end
+
+          def let name, &block
+            define_method name do
+              var_name = "@#{name}"
+
+              if value = instance_variable_get(var_name)
+                value
+              else
+                instance_variable_set var_name, instance_eval(&block)
+              end
             end
+          end
+
+          def letc name, &block
+            define_method name do
+              var_name = "@#{name}"
+
+              if value = instance_variable_get(var_name)
+                value
+              else
+                instance_variable_set var_name, eval_configured(&block)
+              end
+            end
+          end
+
+          def getc name, &block
+            define_method name do
+              eval_configured(&block)
+            end
+          end
+
+          def box name, box_class
+            box_proc = lambda do
+              box_class.new(parent: self)
+            end
+            let(name, &box_proc)
+          end
+        end
+        
+        def eval_configured &block
+          instance_eval(&block).tap do |v|
+            v.config = self
           end
         end
       end
+    end
+
+    define_class 'UsersBox' do
+      include Box
+
+      let(:logger) { Logger.new level: 'INFO' }
+      letc(:api) { UsersApi }
+      getc(:collection) { UsersCollection.new }
+    end
+
+    define_class 'MainBox' do
+      include Box
 
       get(:log_level) { 'INFO' }
       let(:logger) { Logger.new level: log_level }
-      let(:server) { Server.new self }
+      getc(:server) { Server.new }
+      letc(:memoized_server) { Server.new }
+      box(:users, UsersBox)
     end
   end
 
-  let(:users_collection) { UsersCollection.new logger: logger }
+  let(:users_collection) { users_box.collection }
   let(:logger) { box.logger }
 
-  let(:users_api) do
-    UsersApi.tap do |api|
-      api.logger = logger
-    end
-  end
+  let(:users_api) { users_box.api }
 
   let(:box) { MainBox.new }
+  let(:users_box) { box.users }
 
   def log_level
     box.log_level
@@ -100,9 +159,23 @@ RSpec.describe 'Box' do
     end
   end
 
+  describe 'memoized_server' do
+    it 'is configured' do
+      expect(box.memoized_server.logger).to be box.logger
+    end
+
+    it 'is not memoized' do
+      expect(box.memoized_server).to be box.memoized_server
+    end
+  end
+
   describe 'server' do
     it 'is configured' do
       expect(box.server.logger).to be box.logger
+    end
+
+    it 'is not memoized' do
+      expect(box.server).not_to be box.server
     end
   end
 
